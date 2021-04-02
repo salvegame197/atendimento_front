ad1b452f6ba75f646354317719fc769103986d40
Gabriel Demamann <gabrieldemamann@gmail.com>
2021-04-02 13:16:14 -0300

    Created base front end project

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import MyRoute from './MyRoute';
+import Login from '../pages/login';
+import Page404 from '../pages/Page404';
+
+export default function Routes() {
+  return (
+    <>
+      <Switch>
+        <MyRoute exact path="/login" component={Login} />
+        <MyRoute path="*" component={Page404} />
+      </Switch>
+    </>
+  );
+}
---
+++ tech.md
@@ -0,0 +1,21 @@
+## tech
+
+Packages
+YARN
+
+VS.CODE
+Eslint
+Prettier
+Editorconfig
+
+Styled-Components
+React Router DOM
+Router
+History
+MyRoute > Router
+React Toastify
+Axios
+Redux-react
+Redux-saga
+Redux-Persist
+PropTypes
